36da0f2dc031d550aceaec2f423e5b369ab10097
Archana Patel <archana.patel@1Rintern.local>
2023-03-17 16:32:40 +0530

    JavaScript Basic

---
+++ index.js
@@ -0,0 +1,44 @@
+// console.log("date object");
+// const d = new Date();
+// console.log(d);
+// const date = new Date();
+// console.log(date.toDateString());
+
+// date get Method
+// console.log(date.getDate());
+// console.log(date.getDay());
+// console.log(date.getFullYear());
+// console.log(date.getHours());
+// console.log(date.getMilliseconds());
+// console.log(date.getMinutes());
+
+// date set Method
+// const d = new Date();
+// d.setFullYear(2020);
+// console.log(d);
+
+// const d = new Date();
+// d.setDate(d.getDate());
+// console.log(d);
+
+// console.log("Math object");
+// console.log(Math.PI);
+// const a = 4.3;
+// console.log(Math.round(a));
+// console.log(Math.ceil(a));
+// console.log(Math.floor(a));
+// console.log(Math.trunc(a));
+// console.log(Math.pow(8, 2));
+// console.log(Math.sqrt(25));
+
+//Alert
+// alert("This is a message");
+
+//promprt
+// let name = prompt("What is your name?","Guest");
+// console.log(name);
+
+//confirm
+let deletepost = confirm("Do you really want to delete this post?");
+console.log(deletepost);
+
